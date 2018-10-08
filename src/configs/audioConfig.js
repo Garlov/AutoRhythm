d2847ff29b2516476b39c43b2c0abe4c618a5193
Sidaroth <sidaroth@protonmail.com>
2018-10-08 22:42:34 +0200

    Update song data to be accurate.

--- src/configs/audioConfig.js
+++ src/configs/audioConfig.js
@@ -7,13 +7,13 @@ export default {
             PATH: 'assets/audio/open_jam_track_1.ogg',
         },
         BOWERS_WILKINS: {
-            ARTIST: 'WOWA',
+            ARTIST: 'JNGS',
             TITLE: 'Bowers & Wilkins',
             KEY: 'bowers_wilkins',
             PATH: 'assets/audio/bowers_&_wilkins.mp3',
         },
         ALL_ALONE: {
-            ARTIST: 'WOWA',
+            ARTIST: 'Hospitalized',
             TITLE: 'All Alone',
             KEY: 'all_alone',
             PATH: 'assets/audio/all_alone.mp3',
