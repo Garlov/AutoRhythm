b56c9d8371effbc897ef7d6540749c95e68054dd
unknown <oe.nordli@gmail.com>
2018-10-08 20:31:47 +0200

    release prepping and new audio

--- src/configs/audioConfig.js
+++ src/configs/audioConfig.js
@@ -4,7 +4,7 @@ export default {
             ARTIST: 'BleepBloopBleep1',
             TITLE: 'Open Jam Track 1',
             KEY: 'OpenJam',
-            PATH: 'assets/audio/open_jam_track_1.wav',
+            PATH: 'assets/audio/open_jam_track_1.ogg',
         },
         BOWERS_WILKINS: {
             ARTIST: 'WOWA',
@@ -17,12 +17,6 @@ export default {
             TITLE: 'All Alone',
             KEY: 'all_alone',
             PATH: 'assets/audio/all_alone.mp3',
-        },
-        TEST: {
-            ARTIST: 'Audacity',
-            TITLE: 'Test Tunes 10-20k hz',
-            KEY: 'test',
-            PATH: 'assets/audio/test.ogg',
         },
     },
     SFX: {},
